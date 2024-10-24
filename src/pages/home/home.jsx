f7f06ae4d0bff9c3813b5053a5d48db340783baf
Esteban DROUET <Esteban.Drouet94400@gmail.com>
2024-10-25 00:44:15 +0200

    Added keys to mapping

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,22 +1,22 @@
-import Header from '../../components/Header/Header.jsx';
-import Banner from '../../components/Banner/HomeBanner/HomeBanner.jsx';
-import Logement from '../../components/Rental/logement.jsx'
-import Footer from '../../components/Footer/Footer.jsx';
-import jsonData from '../../data/logements.json';
+import Header from "../../components/Header/Header.jsx";
+import Banner from "../../components/Banner/HomeBanner/HomeBanner.jsx";
+import Logement from "../../components/Rental/logement.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
+import jsonData from "../../data/logements.json";
 
 function Home() {
-    return (
-        <div>
-            <Header/>
-            <Banner mode="Home"/> 
-            <div className='Gallery'>
-            {jsonData.map((item) => (
-                 <Logement item={item} mode="Card"/>
-            ))}
-        </div>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Banner mode="Home" />
+      <div className="Gallery">
+        {jsonData.map((item) => (
+          <Logement key={item.id} item={item} mode="Card" />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default Home; 
+export default Home;
